7954812364a7133e382bf36332bfa8aeabfa90c4
AOF_O5-1 <marcusfajemisin@gmail.com>
2025-02-02 01:10:57 +0000

    hid my tokens

--- server/dist/routes/auth-routes.js
+++ server/dist/routes/auth-routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
 export const login = async (req, res) => {
     // TODO: If the user exists and the password is correct, return a JWT 
     const { username, password } = req.body;
